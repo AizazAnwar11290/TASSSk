2488acf9d14141e6349b888e81478eae2c65f85f
AizazAnwar11290 <140271798+AizazAnwar11290@users.noreply.github.com>
2024-04-20 06:23:58 +0500

    Add files via upload

---
+++ TASSSk/index.js
@@ -0,0 +1,24 @@
+"use strict";
+let moviename = ("A&A");
+let firstName = "Aizaz";
+let SecondName = "Anwar";
+let age = 33;
+console.log(`Hi, this is ${firstName} and I am ${age} years old. `);
+const goodMood = true;
+console.log(`Full name: ${firstName} ${SecondName}.`);
+let productName = "1 KG Suger";
+let productPrice = 180;
+console.log(`Product Name: ${productName}
+Price: ${productPrice} RS.`);
+let countpet;
+let birthday = false;
+console.log(`First Name: ${firstName}
+
+Last Name: ${SecondName}
+Age: ${age}`);
+const birthYear = 1990;
+let city = "Gujranwala";
+let country = "Pakistan";
+console.log(`${city}, ${country}.`);
+let temToday = 30;
+console.log(temToday);
---
+++ TASSSk/index.ts
@@ -0,0 +1,31 @@
+let moviename:string=("A&A");
+let firstName="Aizaz";
+let SecondName="Anwar";
+
+let age:number=33;
+
+console.log(`Hi, this is ${firstName} and I am ${age} years old. `);
+
+const goodMood:boolean=true;
+
+console.log(`Full name: ${firstName} ${SecondName}.`);
+
+let productName="1 KG Suger";
+let productPrice= 180;
+console.log(`Product Name: ${productName}
+Price: ${productPrice} RS.`);
+
+let countpet: number;
+let birthday: boolean = false;
+console.log(`First Name: ${firstName}
+
+Last Name: ${SecondName}
+Age: ${age}`);
+const birthYear: number = 1990;
+let city="Gujranwala";
+let country= "Pakistan";
+console.log(`${city}, ${country}.`);
+
+let temToday: number= 30;
+
+console.log(temToday)
